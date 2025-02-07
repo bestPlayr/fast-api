b8f3d7ee7a24ee55f1bb0f84f28fcd3e2feea312
Mesum <messum2004@gamil.com>
2025-02-07 09:25:31 -0800

    mongodb connection

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from fastapi import FastAPI
-
+from fastapi.responses import HTMLResponse
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 app = FastAPI()
 
+load_dotenv()
+
+db_url=os.getenv('MONGODB_URI')
+db_name=os.getenv('DB_NAME')
+
+client=AsyncIOMotorClient(db_url)
+db=client[db_name]
+
+@app.get('/',response_class=HTMLResponse)
+async def read_root():
+    if(await db):
+        return 'yes'
+    return """<h1>Hello</h1> """
