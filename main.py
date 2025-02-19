35a2d7ae56466894b07b901f6f56335249693cf5
Mesum <messum2004@gamil.com>
2025-02-19 07:51:22 -0800

    Added Delete and Update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException,Depends
+from fastapi import FastAPI, HTTPException,Depends,status
 from pydantic import BaseModel
 from pymongo.mongo_client import MongoClient
 import models
@@ -37,4 +37,30 @@ def users_list(db:Session = Depends(get_db)):
     result=db.query(models.User).all()
     return result
 
+@app.get('/user/{id}')
+def get_user_by_id(id,db:Session=Depends(get_db)):
+    return db.query(models.User).filter(models.User.id==id).first()
+
+@app.delete('/user/{id}')
+def delete(id,db:Session = Depends(get_db)):
+    user=db.query(models.User).filter(models.User.id==id).first()
+    if not user:
+        raise HTTPException(status_code=404,detail='No such record found')        
+    db.query(models.User).filter(models.User.id==id).delete(synchronize_session=False)
+    db.commit()
+    return {'message':'Successfully deleted record'}
+
+@app.put('/update-user/{id}')
+def update(id,request:UserSchema,db:Session = Depends(get_db)):
     
+    user=db.query(models.User).filter(models.User.id==id)
+
+    if not user:
+        raise HTTPException(status_code=404,detail='No such record found')
+    
+    user.update(request.model_dump())
+    db.commit()
+    return {'message':'Operation Successfull'}
+
+
+   
